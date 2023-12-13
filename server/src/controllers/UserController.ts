5d51de13f3d3f6d3df4b12ce7adec167811103ae
Thaís Neves <neves.thais@hotmail.com>
2023-12-13 16:12:10 -0300

    node challenge done

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -25,6 +25,44 @@ class UserController implements Crud {
     return response.status(httpStatus).send(values);
   };
 
+  // função que filtra usuários com idade inferior à idade fornecida no get
+  getById = async (request: Request, response: Response) => {
+    const { age } = request.params;
+    const { httpStatus, values } = await this.citi.getAll();
+
+    const valueToReturn = values.filter(value => value.age < Number(age));
+
+    if (valueToReturn.length === 0) return response.status(httpStatus).send('Não há pessoas usuárias para o filtro escolhido');
+
+    return response.status(httpStatus).send(valueToReturn);
+  };
+
+  // função que calcula e retorna a média de idade dos usuários
+  averageAge = async (request: Request, response: Response) => {
+    const { httpStatus, values } = await this.citi.getAll();
+
+    const ageSum = values.reduce((accumulate, value) => {
+      return accumulate + value.age
+    }, 0);
+    const numberOfPeople = values.length
+
+    const average = ageSum / numberOfPeople;
+
+    return response.status(httpStatus).send({ averageAge: average });
+  };
+
+  // função que filtra usuários pelo sobrenome fornecido no get
+  getByLastName = async (request: Request, response: Response) => {
+    const { lastName } = request.params;
+    const { httpStatus, values } = await this.citi.getAll();
+
+    const filteredUsers = values.filter((user) => user.lastName.toLowerCase() === lastName.toLowerCase());
+
+    if (filteredUsers.length === 0) return response.status(httpStatus).send(`Nenhum usuário encontrado com o sobrenome "${lastName}"`);
+
+    return response.status(httpStatus).send(filteredUsers);
+  };
+
   delete = async (request: Request, response: Response) => {
     const { id } = request.params;
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,9 @@ const routes = express.Router();
 
 routes.post("/user", userController.create);
 routes.get("/user", userController.get);
+routes.get("/user/:age", userController.getById);  // rota para filtrar usuários por idade
+routes.get("/average", userController.averageAge); // rota para filtrar a média de idade dos usuários
+routes.get("/user/:lastName", userController.getByLastName); // rota para filtrar usuários pelo sobrenome
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
 
